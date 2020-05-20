635557944aa928475fd3f25e2539152806d87c92
Szejp <janqczmowski@gmail.com>
2020-05-20 14:41:15 +0200

    imported build system

---
+++ Assets/Easy Build System/Samples & Integrations/Samples/Samples Contents/Scripts/ExampleAppearanceChanger.cs
@@ -0,0 +1,22 @@
+﻿using EasyBuildSystem.Runtimes.Internal.Part;
+using UnityEngine;
+
+public class ExampleAppearanceChanger : MonoBehaviour
+{
+    private PartBehaviour part;
+
+    private void Awake()
+    {
+        part = GetComponent<PartBehaviour>();
+
+        InvokeRepeating("UpdateAppearance", 1f, 1f);
+    }
+
+    private void UpdateAppearance()
+    {
+        if (part.AppearanceIndex >= part.Appearances.Count-1)
+            part.AppearanceIndex = 0;
+        else
+            part.AppearanceIndex++;
+    }
+}
